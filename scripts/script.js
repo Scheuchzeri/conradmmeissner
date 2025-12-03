b63bc7cf2b1de9ac3369998654ea543f143117a9
A. Scheuchzeri <conrad.meissner@t-online.de>
2025-12-03 18:09:32 +0100

    Set up some basic functionality and hero-section layout.

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
   console.log("Script recognized.");
+
+  openMobileMenu();
 });
+
+function openMobileMenu() {
+  const mobileMenuButton = document.querySelector(".mobile-menu-button");
+  const heroSection = document.querySelector(".hero-section");
+  const projectsSection = document.querySelector(".projects-section");
+
+  document.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if (!e.target === mobileMenuButton) {
+      console.log("Not the right button.");
+    } else {
+      heroSection.classList.toggle("active");
+      projectsSection.classList.toggle("active");
+    }
+  });
+}
